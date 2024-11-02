041caa69cd021904c06c1646d24ff614b886e9f3
KevinHavn <kevin-havn@hotmail.com>
2024-11-02 16:15:37 +0100

    added logic using formik and yup to validate, and send an alert through the user interface

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -1,91 +1,142 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useFormik } from "formik";
 import {
-  Box,
-  Button,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Heading,
-  Input,
-  Select,
-  Textarea,
-  VStack,
+	Box,
+	Button,
+	FormControl,
+	FormErrorMessage,
+	FormLabel,
+	Heading,
+	Input,
+	Select,
+	Textarea,
+	VStack,
 } from "@chakra-ui/react";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 import FullScreenSection from "./FullScreenSection";
 import useSubmit from "../hooks/useSubmit";
-import {useAlertContext} from "../context/alertContext";
+import { useAlertContext } from "../context/alertContext";
 
 const LandingSection = () => {
-  const {isLoading, response, submit} = useSubmit();
-  const { onOpen } = useAlertContext();
+	const { isLoading, response, submit } = useSubmit();
+	const { onOpen } = useAlertContext();
 
-  const formik = useFormik({
-    initialValues: {},
-    onSubmit: (values) => {},
-    validationSchema: Yup.object({}),
-  });
+	const formik = useFormik({
+		initialValues: { firstName: "", email: "", type: "hireMe", comment: "" },
 
-  return (
-    <FullScreenSection
-      isDarkBackground
-      backgroundColor="#512DA8"
-      py={16}
-      spacing={8}
-    >
-      <VStack w="1024px" p={32} alignItems="flex-start">
-        <Heading as="h1" id="contactme-section">
-          Contact me
-        </Heading>
-        <Box p={6} rounded="md" w="100%">
-          <form>
-            <VStack spacing={4}>
-              <FormControl isInvalid={false}>
-                <FormLabel htmlFor="firstName">Name</FormLabel>
-                <Input
-                  id="firstName"
-                  name="firstName"
-                />
-                <FormErrorMessage></FormErrorMessage>
-              </FormControl>
-              <FormControl isInvalid={false}>
-                <FormLabel htmlFor="email">Email Address</FormLabel>
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                />
-                <FormErrorMessage></FormErrorMessage>
-              </FormControl>
-              <FormControl>
-                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                <Select id="type" name="type">
-                  <option value="hireMe">Freelance project proposal</option>
-                  <option value="openSource">
-                    Open source consultancy session
-                  </option>
-                  <option value="other">Other</option>
-                </Select>
-              </FormControl>
-              <FormControl isInvalid={false}>
-                <FormLabel htmlFor="comment">Your message</FormLabel>
-                <Textarea
-                  id="comment"
-                  name="comment"
-                  height={250}
-                />
-                <FormErrorMessage></FormErrorMessage>
-              </FormControl>
-              <Button type="submit" colorScheme="purple" width="full">
-                Submit
-              </Button>
-            </VStack>
-          </form>
-        </Box>
-      </VStack>
-    </FullScreenSection>
-  );
+		onSubmit: async (values) => {
+			const url = "https://meta.com/something";
+			await submit(url, values);
+		},
+
+		validationSchema: Yup.object({
+			firstName: Yup.string().required("First name is required"),
+			email: Yup.string()
+				.email("Invalid email format")
+				.required("Email is required"),
+			type: Yup.string()
+				.oneOf(["hireMe", "openSource", "other"], "Select a valid inquiry type")
+				.required("Inquiry type is required"),
+			comment: Yup.string()
+				.max(250, "Message cannot exceed 250 characters")
+				.required("Message is required"),
+		}),
+	});
+
+	useEffect(() => {
+		if (response) {
+			if (response.type === "success") {
+				onOpen(
+					"success",
+					`Thank you, ${formik.values.firstName}! Your message has been sent.`
+				);
+				formik.resetForm();
+			} else if (response.type === "error") {
+				onOpen("error", response.message);
+			}
+		}
+	}, [response]);
+
+	return (
+		<FullScreenSection
+			isDarkBackground
+			backgroundColor="#512DA8"
+			py={16}
+			spacing={8}>
+			<VStack w="1024px" p={32} alignItems="flex-start">
+				<Heading as="h1" id="contactme-section">
+					Contact me
+				</Heading>
+				<Box p={6} rounded="md" w="100%">
+					<form onSubmit={formik.handleSubmit}>
+						<VStack spacing={4}>
+							<FormControl
+								isInvalid={formik.touched.firstName && formik.errors.firstName}>
+								<FormLabel htmlFor="firstName">Name</FormLabel>
+								<Input
+									id="firstName"
+									name="firstName"
+									value={formik.values.firstName}
+									onChange={formik.handleChange}
+									onBlur={formik.handleBlur}
+								/>
+								<FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
+							</FormControl>
+							<FormControl
+								isInvalid={formik.touched.email && formik.errors.email}>
+								<FormLabel htmlFor="email">Email Address</FormLabel>
+								<Input
+									id="email"
+									name="email"
+									type="email"
+									value={formik.values.email}
+									onChange={formik.handleChange}
+									onBlur={formik.handleBlur}
+								/>
+								<FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+							</FormControl>
+							<FormControl
+								isInvalid={formik.touched.type && formik.errors.type}>
+								<FormLabel htmlFor="type">Type of enquiry</FormLabel>
+								<Select
+									id="type"
+									name="type"
+									value={formik.values.type}
+									onChange={formik.handleChange}
+									onBlur={formik.handleBlur}>
+									<option value="hireMe">Freelance project proposal</option>
+									<option value="openSource">
+										Open source consultancy session
+									</option>
+									<option value="other">Other</option>
+								</Select>
+							</FormControl>
+							<FormControl
+								isInvalid={formik.touched.comment && formik.errors.comment}>
+								<FormLabel htmlFor="comment">Your message</FormLabel>
+								<Textarea
+									id="comment"
+									name="comment"
+									height={250}
+									value={formik.values.comment}
+									onChange={formik.handleChange}
+									onBlur={formik.handleBlur}
+								/>
+								<FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
+							</FormControl>
+							<Button
+								type="submit"
+								colorScheme="purple"
+								width="full"
+								isLoading={isLoading}>
+								Submit
+							</Button>
+						</VStack>
+					</form>
+				</Box>
+			</VStack>
+		</FullScreenSection>
+	);
 };
 
 export default LandingSection;
